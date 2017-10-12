f11d096a2688c2b670c69f93b519afcb5ff8ff06
Chris Worman <chrisworman@gmail.com>
2017-10-12 14:57:32 -0600

    Initial Commit

---
+++ TaskiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskiOSApp
+//
+//  Created by Chris Worman on 2017-10-12.
+//  Copyright © 2017 Chris Worman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
